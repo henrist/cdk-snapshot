1eb71ef64520ae78484ccb760e963eac04b85732
Henrik Steen <henrist@henrist.net>
2020-11-15 08:12:38 +0100

    test: add cloud assembly v5 specific tests

---
+++ src/snapshot.test.ts
@@ -0,0 +1,207 @@
+import * as execa from "execa"
+import * as fs from "fs"
+import * as rimraf from "rimraf"
+import { createCloudAssemblySnapshot } from "./index"
+import {
+  prepareManifestForSnapshot,
+  prepareTemplateForSnapshot,
+} from "./snapshot"
+
+async function rm(path: string): Promise<void> {
+  return new Promise((resolve) => void rimraf(path, {}, () => resolve()))
+}
+
+describe("A current application", () => {
+  beforeAll(async () => {
+    await rm("cdk.out")
+    await execa("npm", ["run", "cdk", "--", "synth"])
+  }, 30000)
+
+  test("Snapshots should be creatable from CLI", async () => {
+    await rm("cdk.out.test")
+    await execa("node", [
+      "./lib/bin/cdk-snapshot.js",
+      "cdk.out",
+      "cdk.out.test",
+    ])
+
+    const files = fs.readdirSync("cdk.out.test", "utf-8").sort()
+    expect(files).toMatchInlineSnapshot(`
+      Array [
+        "manifest.json",
+        "stack-1.template.json",
+      ]
+    `)
+  }, 30000)
+
+  describe("For a given snapshot", () => {
+    beforeAll(async () => {
+      await rm("cdk.out.test")
+      await createCloudAssemblySnapshot("cdk.out", "cdk.out.test")
+    })
+
+    // To catch any future change in behaviour.
+    test("the cdk.out file should only contain version", () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const content = JSON.parse(fs.readFileSync("cdk.out/cdk.out", "utf-8"))
+      expect(content).toEqual({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        version: expect.any(String),
+      })
+    })
+
+    test("cdk.out version file should be removed", () => {
+      expect(fs.existsSync("cdk.out/cdk.out")).toBe(true)
+      expect(fs.existsSync("cdk.out.test/cdk.out")).toBe(false)
+    })
+
+    test("tree.json should be removed", () => {
+      expect(fs.existsSync("cdk.out/tree.json")).toBe(true)
+      expect(fs.existsSync("cdk.out.test/tree.json")).toBe(false)
+    })
+
+    test("Asset directory should be removed", () => {
+      const filesBefore = fs.readdirSync("cdk.out", "utf-8").sort()
+      expect(filesBefore).toEqual(
+        expect.arrayContaining([expect.stringContaining("asset")]),
+      )
+
+      const filesAfter = fs.readdirSync("cdk.out.test", "utf-8").sort()
+      expect(filesAfter).toEqual(
+        expect.not.arrayContaining([expect.stringContaining("asset")]),
+      )
+    })
+
+    test("Output directory should have expected files", () => {
+      const files = fs.readdirSync("cdk.out.test", "utf-8").sort()
+      expect(files).toMatchInlineSnapshot(`
+      Array [
+        "manifest.json",
+        "stack-1.template.json",
+      ]
+    `)
+    })
+
+    describe("Manifest file", () => {
+      let before: string
+      let after: string
+
+      beforeAll(() => {
+        before = fs.readFileSync("cdk.out/manifest.json", "utf-8")
+        after = fs.readFileSync("cdk.out.test/manifest.json", "utf-8")
+      })
+
+      test("Version should be removed", () => {
+        expect(before).toContain('"version"')
+        expect(after).not.toContain('"version"')
+      })
+
+      test("Runtime information should be removed", () => {
+        expect(before).toContain('"runtime": {\n')
+        expect(after).toContain('"runtime": {}')
+      })
+
+      test("Trace should be removed", () => {
+        expect(before).toContain('"trace"')
+        expect(after).not.toContain('"trace"')
+      })
+
+      test("Asset hashes should be removed", () => {
+        expect(before).toContain("AssetParameters")
+        expect(after).not.toContain("AssetParameters")
+      })
+
+      test("It should still contain resource information", () => {
+        expect(after).toContain('"MyFunctionServiceRole3C357FF2"')
+      })
+
+      test("It should match previous snapshot", () => {
+        expect(after).toMatchSnapshot()
+      })
+    })
+
+    describe("Template file", () => {
+      let before: string
+      let after: string
+
+      beforeAll(() => {
+        before = fs.readFileSync("cdk.out/stack-1.template.json", "utf-8")
+        after = fs.readFileSync("cdk.out.test/stack-1.template.json", "utf-8")
+      })
+
+      test("Asset hashes should be removed", () => {
+        expect(before).toContain("AssetParameters")
+        expect(after).not.toContain("AssetParameters")
+      })
+
+      test("It should match previous snapshot", () => {
+        expect(after).toMatchSnapshot()
+      })
+    })
+  })
+})
+
+describe("Files from a v5 Cloud Assembly", () => {
+  describe("Manifest file", () => {
+    let before: string
+    let after: string
+
+    beforeAll(() => {
+      before = fs.readFileSync(
+        "src/__test_assets__/cloud-assembly-v5/manifest.json",
+        "utf-8",
+      )
+      after = prepareManifestForSnapshot(before)
+    })
+
+    test("Version should be removed", () => {
+      expect(before).toContain('"version"')
+      expect(after).not.toContain('"version"')
+    })
+
+    test("Runtime information should be removed", () => {
+      expect(before).toContain('"runtime": {\n')
+      expect(after).toContain('"runtime": {}')
+    })
+
+    test("Trace should be removed", () => {
+      expect(before).toContain('"trace"')
+      expect(after).not.toContain('"trace"')
+    })
+
+    test("Asset hashes should be removed", () => {
+      expect(before).toContain("AssetParameters")
+      expect(after).not.toContain("AssetParameters")
+    })
+
+    test("It should still contain resource information", () => {
+      expect(after).toContain('"MyFunctionServiceRole3C357FF2"')
+    })
+
+    test("It should match previous snapshot", () => {
+      expect(after).toMatchSnapshot()
+    })
+  })
+
+  describe("Template file", () => {
+    let before: string
+    let after: string
+
+    beforeAll(() => {
+      before = fs.readFileSync(
+        "src/__test_assets__/cloud-assembly-v5/stack-1.template.json",
+        "utf-8",
+      )
+      after = prepareTemplateForSnapshot(before)
+    })
+
+    test("Asset hashes should be removed", () => {
+      expect(before).toContain("AssetParameters")
+      expect(after).not.toContain("AssetParameters")
+    })
+
+    test("It should match previous snapshot", () => {
+      expect(after).toMatchSnapshot()
+    })
+  })
+})
---
+++ src/snapshot.ts
@@ -0,0 +1,242 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import * as cpy from "cpy"
+import * as del from "del"
+import * as fs from "fs"
+import * as path from "path"
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function removeVersion(data: any): any {
+  const cp = {
+    ...data,
+  }
+
+  delete cp.version
+  return cp
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function removeTrace(data: any): any {
+  if (data instanceof Array) {
+    return data.map(removeTrace)
+  }
+
+  if (data === Object(data)) {
+    return Object.fromEntries(
+      Object.entries(data)
+        .filter(([key]) => key !== "trace")
+        .map(([key, value]) => [key, removeTrace(value)]),
+    )
+  }
+
+  return data
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function removeRuntimeLibraries(data: any): any {
+  const cp = {
+    ...data,
+    runtime: {
+      ...data.runtime,
+    },
+  }
+
+  delete cp.runtime.libraries
+  return cp
+}
+
+const currentVersionRegex = /^(.+CurrentVersion[0-9A-F]{8})[0-9a-f]{32}$/
+
+/**
+ * Match a resource created by `lambda.Function.currentVersion`, which
+ * will include the asset hash as part of the reousrce name and return
+ * a snapshot-friendly version of it if found.
+ */
+function rewriteCurrentVersionIfFound(value: string): string | null {
+  const match = currentVersionRegex.exec(value)
+  return match ? `${match[1]}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx` : null
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function removeAssetDetailsFromTemplate(data: any): any {
+  if (data instanceof Array) {
+    return data.map(removeAssetDetailsFromTemplate)
+  }
+
+  if (data === Object(data)) {
+    return Object.fromEntries(
+      Object.entries(data)
+        .map(([key, value]) => {
+          const newCurrentVersion = rewriteCurrentVersionIfFound(key)
+          if (newCurrentVersion) {
+            return [newCurrentVersion, removeAssetDetailsFromTemplate(value)]
+          } else if (key.includes("AssetParameter")) {
+            return null
+          } else if (
+            key === "Ref" &&
+            typeof value === "string" &&
+            value.includes("AssetParameters")
+          ) {
+            return [key, "snapshot-value"]
+          } else if (
+            key === "aws:asset:path" &&
+            typeof value === "string" &&
+            value.startsWith("asset.")
+          ) {
+            return [key, "asset.snapshot-value"]
+          } else {
+            return [key, removeAssetDetailsFromTemplate(value)]
+          }
+        })
+        .filter((it): it is [] => it != null),
+    )
+  }
+
+  if (typeof data === "string") {
+    const newCurrentVersion = rewriteCurrentVersionIfFound(data)
+    if (newCurrentVersion) {
+      return newCurrentVersion
+    }
+  }
+
+  return data
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function removeAssetDetailsFromManifest(data: any): any {
+  if (data instanceof Array) {
+    return data.map(removeAssetDetailsFromManifest)
+  }
+
+  if (data === Object(data)) {
+    // aws:cdk:asset in metadata
+    if (data["type"] === "aws:cdk:asset" && "data" in data) {
+      return {
+        ...data,
+        data: "snapshot-value",
+      }
+    }
+
+    return Object.fromEntries(
+      Object.entries(data)
+        .map(([key, value]) => {
+          if (key.includes("AssetParameters")) {
+            return null
+          } else {
+            return [key, removeAssetDetailsFromManifest(value)]
+          }
+        })
+        .filter((it): it is [] => it != null),
+    )
+  }
+
+  if (typeof data === "string") {
+    const newCurrentVersion = rewriteCurrentVersionIfFound(data)
+    if (newCurrentVersion) {
+      return newCurrentVersion
+    }
+  }
+
+  return data
+}
+
+/**
+ * Remove the CDKMetadata resources that is part of the synthesized
+ * template since CDK 1.63.0.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function removeCdkMetadataResourceFromTemplate(data: any): any {
+  const cp = {
+    ...data,
+    Resources: {
+      ...data.Resources,
+    },
+  }
+
+  delete cp.Resources.CDKMetadata
+  return cp
+}
+
+export function prepareManifestForSnapshot(content: string): string {
+  const input = JSON.parse(content)
+  const output = [
+    removeVersion,
+    // Remove the runtime version information so it don't conflict with CI
+    // or other users generating the snapshots.
+    removeRuntimeLibraries,
+    // Remove the trace from manifest for now.
+    removeTrace,
+    // Avoid details (hashes) from assets.
+    removeAssetDetailsFromManifest,
+  ].reduce((acc, fn) => fn(acc), input)
+
+  return JSON.stringify(output, undefined, "  ")
+}
+
+/**
+ * Transform the Cloud Assembly manifest file so that it can be persisted
+ * as a snapshot without causing invalidations for every synthesize.
+ */
+async function prepareManifestFileForSnapshot(file: string): Promise<void> {
+  const result = prepareManifestForSnapshot(
+    await fs.promises.readFile(file, "utf8"),
+  )
+
+  await fs.promises.writeFile(file, result)
+}
+
+export function prepareTemplateForSnapshot(content: string): string {
+  const input = JSON.parse(content)
+  const output = [
+    removeCdkMetadataResourceFromTemplate,
+    // Avoid details (hashes) from assets.
+    removeAssetDetailsFromTemplate,
+  ].reduce((acc, fn) => fn(acc), input)
+
+  return JSON.stringify(output, undefined, "  ")
+}
+
+/**
+ * Transform a Cloud Assembly template file so that it can be persisted
+ * as a snapshot without causing invalidations for minor changes.
+ */
+async function prepareTemplateFileForSnapshot(file: string): Promise<void> {
+  const result = prepareTemplateForSnapshot(
+    await fs.promises.readFile(file, "utf8"),
+  )
+
+  await fs.promises.writeFile(file, result)
+}
+
+/**
+ * Convert a Cloud Assembly to a snapshot.
+ */
+export async function createCloudAssemblySnapshot(
+  src: string,
+  dst: string,
+): Promise<void> {
+  await cpy(".", path.join(process.cwd(), dst), {
+    parents: true,
+    cwd: path.join(process.cwd(), src),
+  })
+
+  // Transform the manifest to be more snapshot friendly.
+  await prepareManifestFileForSnapshot(path.join(dst, "manifest.json"))
+
+  // Don't keep track of manifest version.
+  await del(path.join(dst, "cdk.out"))
+
+  // The tree file doesn't give us much value as part of the snapshot.
+  await del(path.join(dst, "tree.json"))
+
+  // Remove asset contents for now.
+  await del(path.join(dst, "asset.*"))
+
+  // Transform all templates.
+  for (const file of fs.readdirSync(dst, "utf-8")) {
+    if (file.endsWith("template.json")) {
+      await prepareTemplateFileForSnapshot(path.join(dst, file))
+    }
+  }
+}
